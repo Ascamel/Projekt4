ca1eb870324d66f48a23f563b3c80b32b3c89de9
Ascamel <sebastiankondraciuk3@gmail.com>
2022-11-06 20:12:08 +0100

    working sobel

--- Projekt4/MainWindow.xaml.cs
+++ Projekt4/MainWindow.xaml.cs
@@ -2,8 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,9 +17,14 @@ using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Color = System.Windows.Media.Color;
+using Encoder = System.Drawing.Imaging.Encoder;
+using Image = System.Drawing.Image;
+using Path = System.IO.Path;
+using Rectangle = System.Drawing.Rectangle;
 
 namespace Projekt4
 {
@@ -238,7 +246,7 @@ namespace Projekt4
             }
             else if(SobelButton.IsChecked.GetValueOrDefault())
             {
-
+                SobelImage();
             }
             else if (HighPassButton.IsChecked.GetValueOrDefault())
             {
@@ -252,6 +260,104 @@ namespace Projekt4
             {
 
             }
+        }
+
+
+        string imgPath;
+        private void SobelImage()
+        {
+            Bitmap original = new(imgPath);
+
+            Bitmap b = original;
+            Bitmap bb = original;
+            int width = b.Width;
+            int height = b.Height;
+            int[,] gx = new int[,] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+            int[,] gy = new int[,] { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+
+            int[,] allPixR = new int[width, height];
+            int[,] allPixG = new int[width, height];
+            int[,] allPixB = new int[width, height];
+
+            int limit = 128 * 128;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    allPixR[i, j] = b.GetPixel(i, j).R;
+                    allPixG[i, j] = b.GetPixel(i, j).G;
+                    allPixB[i, j] = b.GetPixel(i, j).B;
+                }
+            }
+
+            int new_rx = 0, new_ry = 0;
+            int new_gx = 0, new_gy = 0;
+            int new_bx = 0, new_by = 0;
+            int rc, gc, bc;
+            for (int i = 1; i < b.Width - 1; i++)
+            {
+                for (int j = 1; j < b.Height - 1; j++)
+                {
+
+                    new_rx = 0;
+                    new_ry = 0;
+                    new_gx = 0;
+                    new_gy = 0;
+                    new_bx = 0;
+                    new_by = 0;
+                    rc = 0;
+                    gc = 0;
+                    bc = 0;
+
+                    for (int wi = -1; wi < 2; wi++)
+                    {
+                        for (int hw = -1; hw < 2; hw++)
+                        {
+                            rc = allPixR[i + hw, j + wi];
+                            new_rx += gx[wi + 1, hw + 1] * rc;
+                            new_ry += gy[wi + 1, hw + 1] * rc;
+
+                            gc = allPixG[i + hw, j + wi];
+                            new_gx += gx[wi + 1, hw + 1] * gc;
+                            new_gy += gy[wi + 1, hw + 1] * gc;
+
+                            bc = allPixB[i + hw, j + wi];
+                            new_bx += gx[wi + 1, hw + 1] * bc;
+                            new_by += gy[wi + 1, hw + 1] * bc;
+                        }
+                    }
+                    if (new_rx * new_rx + new_ry * new_ry > limit || new_gx * new_gx + new_gy * new_gy > limit || new_bx * new_bx + new_by * new_by > limit)
+                        bb.SetPixel(i, j, System.Drawing.Color.Black);
+
+                    else
+                        bb.SetPixel(i, j, System.Drawing.Color.Transparent);
+                }
+            }
+
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+            encoderParameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 100L);
+
+            string FileName2 = Path.Combine(Environment.CurrentDirectory, @"tmp.jpg");
+
+            bb.Save(FileName2, GetEncoder(ImageFormat.Jpeg), encoderParameters);
+
+            MyImage2.Source = new BitmapImage(new Uri(FileName2));
+        }
+
+        public static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+
+            return null;
         }
         public static int Median(byte[] data)
         {
@@ -381,7 +487,8 @@ namespace Projekt4
               "Portable Network Graphic (*.png)|*.png";
             if (op.ShowDialog() == true)
             {
-                MyImage2.Source = new BitmapImage(new Uri(op.FileName));
+                imgPath = op.FileName;
+                MyImage2.Source = new BitmapImage(new Uri(imgPath));
             }
         }
     }
